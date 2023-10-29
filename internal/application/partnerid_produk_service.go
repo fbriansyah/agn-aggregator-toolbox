1811a285f90c8b37b9ec756297c0b7dbf492f3f3
Febriansyah <fbriansyah@gmail.com>
2023-10-29 16:39:43 +0700

    add CreatePartnerProduk to service

--- internal/application/partnerid_produk_service.go
+++ internal/application/partnerid_produk_service.go
@@ -3,6 +3,7 @@ package application
 import (
 	"context"
 
+	"github.com/fbriansyah/agn-aggregator-toolbox/internal/adapter/mariadb"
 	"github.com/fbriansyah/agn-aggregator-toolbox/internal/application/domain"
 	"github.com/fbriansyah/agn-aggregator-toolbox/util"
 )
@@ -22,4 +23,23 @@ func (s *Service) GetListPartnerProduk(ctx context.Context, idproduk int32) ([]d
 	}
 
 	return listPartnerProduk, nil
+}
+
+func (s *Service) CreatePartnerProduk(ctx context.Context, data domain.PartnerProdukDomain) (domain.PartnerProdukDomain, error) {
+	result, err := s.db.CreatePartnerIDProduk(ctx, mariadb.CreatePartnerIDProdukParams{
+		Idproduk:   util.ValidNullInt32(data.Idproduk),
+		Idmerchant: util.ValidNullInt32(data.Idmerchant),
+		Idpartner:  util.ValidNullInt32(data.Idpartner),
+		Prioritas:  util.ValidNullInt32(data.Prioritas),
+	})
+	if err != nil {
+		return domain.PartnerProdukDomain{}, err
+	}
+
+	id, err := result.LastInsertId()
+	if err == nil {
+		data.IdpartnerProduk = int32(id)
+	}
+
+	return data, nil
 }
