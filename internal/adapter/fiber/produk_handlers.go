44dad385dcd9b63098580853e76394d0fe8ad6af
Febriansyah <fbriansyah@gmail.com>
2023-11-01 19:07:15 +0700

    add get-products route

--- internal/adapter/fiber/produk_handlers.go
+++ internal/adapter/fiber/produk_handlers.go
@@ -2,7 +2,9 @@ package fiber
 
 import (
 	"context"
+	"time"
 
+	"github.com/fbriansyah/agn-aggregator-toolbox/internal/application/domain"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -17,4 +19,23 @@ func (a *FiberAdapter) ListProduk(c *fiber.Ctx) error {
 		"AddButtonUrl": "/add-product",
 		"Products":     products,
 	}, "partials/base")
+}
+
+func (a *FiberAdapter) GetProducts(c *fiber.Ctx) error {
+	kodeProduk := c.Query("kode-produk")
+	namaProduk := c.Query("nama-produk")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
+	defer cancel()
+
+	products, err := a.service.GetProducts(ctx, domain.ProductDomain{
+		KodeProduk: kodeProduk,
+		NamaProduk: namaProduk,
+	})
+	if err != nil {
+		return err
+	}
+	return c.Render("pages/product/list-product", fiber.Map{
+		"Products": products,
+	})
 }
--- internal/adapter/fiber/route.go
+++ internal/adapter/fiber/route.go
@@ -2,4 +2,5 @@ package fiber
 
 func (a *FiberAdapter) router() {
 	a.app.Get("/", a.ListProduk)
+	a.app.Get("/get-products", a.GetProducts)
 }
