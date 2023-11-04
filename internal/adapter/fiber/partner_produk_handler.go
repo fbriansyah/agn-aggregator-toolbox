b4d853a707516456b44054f242b428ef3b5d559d
Febriansyah <fbriansyah@gmail.com>
2023-11-04 16:34:49 +0700

    add list partner route handler

--- internal/adapter/fiber/partner_produk_handler.go
+++ internal/adapter/fiber/partner_produk_handler.go
@@ -1,6 +1,12 @@
 package fiber
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 // GetPartnerProdukForm render pages/partner-produk/create-form.html
 func (a *FiberAdapter) GetPartnerProdukForm(c *fiber.Ctx) error {
@@ -10,12 +16,29 @@ func (a *FiberAdapter) GetPartnerProdukForm(c *fiber.Ctx) error {
 	})
 }
 
+// GetListPartnerProduk render pages/partner-produk/list-partner
 func (a *FiberAdapter) GetListPartnerProduk(c *fiber.Ctx) error {
-	// id := c.Query("idproduk")
+	id := c.Query("idproduk")
 
-	return fiber.ErrNotFound
+	idproduk, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		return err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
+	defer cancel()
+
+	partnerProduks, err := a.service.GetListPartnerProduk(ctx, int32(idproduk))
+	if err != nil {
+		return err
+	}
+
+	return c.Render("pages/partner-produk/list-partner", fiber.Map{
+		"Idproduk": id,
+		"Partners": partnerProduks,
+	})
 }
 
+// GetPartnerProdukEditForm render templates/chi/pages/partner-produk/edit-form.html
 func (a *FiberAdapter) GetPartnerProdukEditForm(c *fiber.Ctx) error {
 	// id := c.Query("id")
 
