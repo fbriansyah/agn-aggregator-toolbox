4e4abcfe820080ab50002f9c316a96a2a2eccbf7
Febriansyah <fbriansyah@gmail.com>
2023-11-06 08:32:36 +0700

    add comment

--- util/database.go
+++ util/database.go
@@ -28,26 +28,32 @@ type QueryWhere struct {
 	Wheres []string
 }
 
+// Like create like condition (e.g. field LIKE '%value%').
 func (q *QueryWhere) Like(field string, value string) {
 	q.Wheres = append(q.Wheres, field+" LIKE '%"+value+"%'")
 }
 
+// Equal create equality condition (e.g. field = 'value').
 func (q *QueryWhere) Equal(field string, value any) {
 	q.Wheres = append(q.Wheres, fmt.Sprintf("%s = '%v'", field, value))
 }
 
+// Greater create greater check (e.g. field > value)
 func (q *QueryWhere) Greater(field string, value any) {
 	q.Wheres = append(q.Wheres, fmt.Sprintf("%s > %v", field, value))
 }
 
+// Lest create less than check (e.g. field < value)
 func (q *QueryWhere) Less(field string, value any) {
 	q.Wheres = append(q.Wheres, fmt.Sprintf("%s < %v", field, value))
 }
 
+// IsEmpty check if the query container empty or not
 func (q *QueryWhere) IsEmpty() bool {
 	return len(q.Wheres) < 1
 }
 
+// Build building query, join the where array with ' AND '.
 func (q *QueryWhere) Build() string {
 	return strings.Join(q.Wheres, " AND ")
 }
